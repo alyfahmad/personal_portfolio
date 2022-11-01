1a7ce3d5812c052c98836785287ecfa9666066fa
alif <alyfahmad@gmail.com>
2022-11-01 22:48:38 +0600

    fix css of navbar and footer

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -37,17 +37,18 @@ export const NavBar = () => {
   return (
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
-        <Navbar.Brand href="#home">
-            <img style={{  width:60, height:60}} src={A_logo} alt="Logo"/>
+        <Navbar.Brand>
+            <img class="brand_logo" src={A_logo} alt="Logo"/>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" >
+        <Navbar.Toggle aria-controls="responsive-navbar-nav">
             <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')} >Home</Nav.Link>
             <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}  onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
             <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}  onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            <Nav.Link href="#experience" className={activeLink === 'experience' ? 'active navbar-link' : 'navbar-link'}  onClick={() => onUpdateActiveLink('experience')}>Work Experience</Nav.Link>
           </Nav>
           <span class="navbar-text">
               <div className="social-icon">

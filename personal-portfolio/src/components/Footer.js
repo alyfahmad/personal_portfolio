371a556e0146ebfb21ef9cdee5b1ca33e2deb786
alif <alyfahmad@gmail.com>
2022-11-01 21:21:25 +0600

    Fix Footer

--- personal-portfolio/src/components/Footer.js
+++ personal-portfolio/src/components/Footer.js
@@ -11,11 +11,11 @@ export const Footer = () => {
         <footer className="footer">
             <Container class="row no-gutters">
                 <Row>
-                    <Col sm={6} className="align-item-start">
-                    <img style={{ marginLeft: 15, width:50, height:50}} src={A_logo} alt="Logo"></img>
+                    <Col xs ={4} sm={6} className="align-item-start">
+                    <img src={A_logo} alt="Logo"></img>
                     </Col>
-                    <Col sm={6} className="text-end text sm-end">
-                        <div style={{ marginTop: 5 }} className="social-icon">
+                    <Col xs ={8} sm={6} className="text-end text sm-end">
+                        <div className="social-icon">
                             <a href="https://www.linkedin.com/in/alifahmad/" target="_blank"><img src={navIcon1} alt="LinkedIn"/></a>
                             {/* <a href="https://www.facebook.com/AlyfAhmad/" target="_blank"><img src={navIcon2} alt="Facebook"/></a> */}
                             <a href="https://www.instagram.com/alyf_ahmad/" target="_blank"><img src={navIcon3} alt="Instagram"/></a>

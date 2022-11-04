ce29501f02d1431e07f7b57a77c903ec90a710db
alif <alyfahmad@gmail.com>
2022-11-04 17:09:30 +0600

    Optimize for cellphone

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -40,15 +40,15 @@ export const NavBar = () => {
         <Navbar.Brand>
             <img class="brand_logo" src={A_logo} alt="Logo"/>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" >
             <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')} >Home</Nav.Link>
             <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}  onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}  onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
             <Nav.Link href="#experience" className={activeLink === 'experience' ? 'active navbar-link' : 'navbar-link'}  onClick={() => onUpdateActiveLink('experience')}>Work Experience</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}  onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
           </Nav>
           <span class="navbar-text">
               <div className="social-icon">
--- personal-portfolio/src/components/Projects.js
+++ personal-portfolio/src/components/Projects.js
@@ -62,13 +62,13 @@ export const Projects = () => {
                 <Row>
                     <Col>
                         <h2>Projects</h2><br/>
-                        <Tab.Container id="projects-tabs" defaultActiveKey="second">
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
                             <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                                 <Nav.Item>
-                                    <Nav.Link eventKey='first'>Data Quality Assurance</Nav.Link>
+                                    <Nav.Link eventKey='first'>Software Test Automation</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Nav.Link eventKey='second'>Test Automation</Nav.Link>
+                                    <Nav.Link eventKey='second'>Data Quality Assurance</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
                                     <Nav.Link eventKey='third'>Web Development</Nav.Link>
